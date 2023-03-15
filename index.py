c21c07ddcf71dc6a686f258a0e712231fd1f1d3a
Guy Wilhite <108310622+guywil@users.noreply.github.com>
2023-03-15 00:24:32 -0700

    first try

---
+++ index.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://row52.com/Search/?YMMorVin=YMM&Year=2000-2005&V1=&V2=&V3=&V4=&V5=&V6=&V7=&V8=&V9=&V10=&V11=&V12=&V13=&V14=&V15=&V16=&V17=&ZipCode=95023&Page=1&ModelId=1150&MakeId=90&LocationId=&IsVin=false&Distance=100"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+car_elements = soup.find_all("div", class_="list-row")
+
+for car_element in car_elements:
+    vin_element = car_element.find("a", itemprop_="name")
+
+print(vin_element)
+#does not work :(
