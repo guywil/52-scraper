de787507679467e00aa36ebb876310f3daee371b
Guy Wilhite <108310622+guywil@users.noreply.github.com>
2023-06-03 21:28:12 -0700

    all_vin_work

--- index.py
+++ index.py
@@ -1,15 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://row52.com/Search/?YMMorVin=YMM&Year=2000-2005&V1=&V2=&V3=&V4=&V5=&V6=&V7=&V8=&V9=&V10=&V11=&V12=&V13=&V14=&V15=&V16=&V17=&ZipCode=95023&Page=1&ModelId=1150&MakeId=90&LocationId=&IsVin=false&Distance=100"
-page = requests.get(URL)
+URL = "https://row52.com/Search/?YMMorVin=YMM&Year=2001-2005&V1=&V2=&V3=&V4=&V5=&V6=&V7=&V8=&V9=&V10=&V11=&V12=&V13=&V14=&V15=&V16=&V17=&ZipCode=95023&Page=1&ModelId=1150&MakeId=90&LocationId=&IsVin=false&Distance=100"
 
+page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 
-car_elements = soup.find_all("div", class_="list-row")
+# only prints first vin off page
+#car_vin = soup.find('a', {'class':'vin'}).get_text()
 
-for car_element in car_elements:
-    vin_element = car_element.find("a", class_="vin")
+#for word in soup.find_all('vin'):
 
-print(vin_element.text.strip())
-print()
+#    car_vin = word.get_text()
+#car_vin = word.get_text()
+## kinda works
+car_vin = soup.find_all(class_='vin')
+
+if car_vin == 0:
+    print('No vins available at the moment')
+else:
+    print(car_vin.text)
